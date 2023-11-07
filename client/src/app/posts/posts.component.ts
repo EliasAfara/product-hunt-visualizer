66084d3f7e3d57b4cc616721f842c059036a1224
Elias Afara <afaraelias@gmail.com>
2023-11-07 03:33:24 +0100

    Add chart to display number of posts in each topic

--- client/src/app/posts/posts.component.ts
+++ client/src/app/posts/posts.component.ts
@@ -1,6 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { GET_POSTS } from '../graphql.operations';
+import { ChartComponent } from 'ng-apexcharts';
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 const POSTS_PER_FETCH = 20;
 
@@ -47,6 +61,10 @@ interface TopicConnection {
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  @ViewChild('chart')
+  chart: ChartComponent | any;
+  public chartOptions: Partial<ChartOptions> | any;
+
   posts: PostEdge[] = [];
   error: any;
   last_post: string = '';
@@ -82,10 +100,31 @@ export class PostsComponent implements OnInit {
         // Update the cursor for the next fetch
         this.last_post = data.posts.edges[data.posts.edges.length - 1].cursor;
 
+        // Count the topics
+        this.countTopics(data.posts.edges);
+
         // Complete the loading process
         this.toggleLoading();
       });
   }
+
+  countTopics = (data: PostEdge[]) => {
+    data.forEach((post) => {
+      const topics = post.node.topics.edges;
+      topics.forEach((topic) => {
+        const topicName = topic.node.name;
+        // If topicName is not in topics, add it and set it to 1 else increment it by 1
+        this.topics[topicName] = this.topics[topicName]
+          ? this.topics[topicName] + 1
+          : 1;
+      });
+    });
+
+    this.chartOptions.labels = Object.keys(this.topics);
+    this.chartOptions.series = Object.values(this.topics);
+
+    console.log(this.topics);
+  };
 
   // Append more posts data when scrolling
   appendPostsData() {
@@ -110,6 +149,9 @@ export class PostsComponent implements OnInit {
         // Update the cursor for the next fetch
         this.last_post = data.posts.edges[data.posts.edges.length - 1].cursor;
 
+        // Count the topics
+        this.countTopics(this.posts);
+
         // Complete the loading process
         this.toggleLoading();
         this.isFetchingData = false; // Reset the flag
@@ -121,7 +163,52 @@ export class PostsComponent implements OnInit {
     this.appendPostsData();
   }
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo) {
+    this.chartOptions = {
+      series: [],
+      chart: {
+        width: 400,
+        type: 'donut',
+      },
+      labels: [],
+      responsive: [
+        {
+          breakpoint: 2500,
+          options: {
+            chart: {
+              width: 700,
+            },
+            legend: {
+              position: 'right',
+            },
+          },
+        },
+      ],
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              total: {
+                show: true,
+                showAlways: true,
+                label: 'Total Topics Count',
+                fontSize: '22px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                fontWeight: 600,
+                color: '#373d3f',
+                formatter: function (w: { globals: { seriesTotals: any[] } }) {
+                  return w.globals.seriesTotals.reduce((a: any, b: any) => {
+                    return a + b;
+                  }, 0);
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+  }
 
   ngOnInit(): void {
     // Initial data fetch

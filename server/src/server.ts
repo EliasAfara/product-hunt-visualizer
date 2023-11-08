53672005edb0ea80a366a0f5432fda8c63d917cf
Elias Afara <afaraelias@gmail.com>
2023-11-08 19:50:59 +0100

    Add ApolloServerPluginLandingPageDisabled plugin to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
+
 import { typeDefs } from "./graphql.js";
 import { resolvers } from "./resolvers.js";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginLandingPageDisabled()],
 });
 
 export default server;
